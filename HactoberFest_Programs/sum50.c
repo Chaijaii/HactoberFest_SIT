3fada219d433d51584254b2bb85131bf655573c7
Chaijaii <57027123+Chaijaii@users.noreply.github.com>
2019-10-26 11:17:37 +0530

    edited version

--- HactoberFest_Programs/sum50.c
+++ HactoberFest_Programs/sum50.c
@@ -2,21 +2,21 @@
 int main(){
 	int arr[100];
 	int i,sum=0;
-	for(i=0;i<100;i++){
+	for(i=0;i<=100;i++){
 		if(i<50)
 			arr[i]=i;
 		else
-			arr[i]=100-i/2;
+			arr[i]=i;
 	}
 	printf("The array is: \n");
-	for(i=0;i<100;i++){
+	for(i=0;i<=100;i++){
 		if(i%10==0)
 			printf("\n");
 		printf("%d\t",arr[i]);
 	}
 	int j;
-	for(i=0;i<100;i++){
-		for(j=i+1;j<100;j++){
+	for(i=0;i<=100;i++){
+		for(j=i+1;j<=100;j++){
 			if(arr[i]+arr[j]==50){
 				printf("\narr[%d][%d]=[%d,%d]\n",i,j,arr[i],arr[j]);
 			}
